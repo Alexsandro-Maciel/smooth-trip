b7d0d5bad3006ba5566976685a3f8235777b482e
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-19 14:31:01 -0300

    Criação da tela de escolha de tipo de usuario

---
+++ smooth-trip.View/frmEscolherTipo.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace smooth_trip
+{
+    public partial class frmEscolherTipo : Window
+    {
+        public frmEscolherTipo()
+        {
+            InitializeComponent();
+        }
+
+        private void VoltarParaFrmLogin(object sender, MouseButtonEventArgs e)
+        {
+            frmLogin frmLogin = new frmLogin();
+            frmLogin.Show();
+            Close();
+        }
+
+        private void Fechar(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void ConfirmarTipoUsuario(object sender, MouseButtonEventArgs e)
+        {
+            frmCadastroFazendeiro frmCadastroFazendeiro = new frmCadastroFazendeiro();
+            frmCadastroFazendeiro.Show();
+            Close();
+        }
+    }
+}
