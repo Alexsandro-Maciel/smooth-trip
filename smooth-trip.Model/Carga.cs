83a25963b6618ebbf19b94de2b810095f4ff9c2f
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-28 14:49:16 -0300

    Correção de erros

--- smooth-trip.Model/Carga.cs
+++ smooth-trip.Model/Carga.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class carga
+public class Carga
 {
     public int Id { get; set; }
     public string Endereco_Destino { get; set; }
