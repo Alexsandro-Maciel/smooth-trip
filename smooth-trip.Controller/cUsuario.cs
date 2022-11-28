0acac8d9436115e50464aab7561663352dc9070c
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-27 22:54:41 -0300

    Atualização da biblioteca

--- smooth-trip.Controller/cUsuario.cs
+++ smooth-trip.Controller/cUsuario.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 public static class cUsuario
 {
+    public static Usuario ObterUsuarioPeloNomeSenha(string nomeUsuario, string senha)
+    {
+        return ConsultasUsuario.ObterUsuarioPeloNomeSenha(nomeUsuario, senha);
+    }
 
+    public static bool VerificarUsuarioExistente(string nomeUsuario)
+    {
+        return ConsultasUsuario.VerificarUsuarioExistente(nomeUsuario);
+    }
+
+    public static bool NovoUsuario(string nomeUsuario, string senha, string email, string tipoUsuario)
+    {
+        return ConsultasUsuario.NovoUsuario(nomeUsuario, senha, email, tipoUsuario);
+    }
 }
