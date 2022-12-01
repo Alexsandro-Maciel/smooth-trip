f1da700c3ed75a84f887395b657af96e90ad96e5
Alexsandro-Maciel <alexsandromaciel.m@gmail.com>
2022-11-30 21:22:49 -0300

    Criação do método de envio de emails

---
+++ smooth-trip.View/EnviarEmail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class EnviarEmails
+{
+    public static string EnviarEmail(string email, string nomeUsuario)
+    {
+        Random random = new Random();
+        string codigo = random.Next(100000, 1000000).ToString();
+
+
+
+        return codigo;
+    }
+}
